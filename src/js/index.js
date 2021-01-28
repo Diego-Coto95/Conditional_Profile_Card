4e3b553ef9ede914a6e411ffda09fb00eec354a2
Juan Diego Coto <jd506@hotmail.com>
2021-01-28 02:03:51 +0000

    Adding validation in null fields

--- src/js/index.js
+++ src/js/index.js
@@ -28,19 +28,33 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
+  if (variables.name === null) {
+    variables.name = "María";
+  }
+  if (variables.lastname === null) {
+    variables.lastname = "Rodríguez";
+  }
+  if (variables.role === null) {
+    variables.role = "Web Developer";
+  }
+  if (variables.city === null) {
+    variables.city = "Cartago, Dulce Nombre";
+  }
+  if (variables.country === null) {
+    variables.country = "Costa Rica";
+  }
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}&nbsp;${variables.lastname}</h1>
+          <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
-          <h3>${variables.city}&nbsp;${variables.country}</h3>
-          <ul class="position-right">
-            <li><a href="${variables.twitter}><i class="fa fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -61,7 +75,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "Diego-Coto95",
     linkedin: null,
     instagram: null,
     name: null,
